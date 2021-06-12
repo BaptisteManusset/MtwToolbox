bb992a8bb82a53b96dc74ab0cfb457ae72d2d20b
Baptiste <baptiste.manusset@gmail.com>
2021-06-12 22:35:15 +0200

    singletonScriptable

---
+++ SingletonScriptableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using UnityEngine;
+
+//https://medium.com/@GilbertoBitt/singleton-scriptableobject-madewithunity-bfe9b8385566
+public abstract class SingletonScriptableObject<T> : ScriptableObject where T : ScriptableObject {
+    static T _instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if (!_instance)
+                _instance = Resources.FindObjectsOfTypeAll<T>().FirstOrDefault();
+            return _instance;
+        }
+    }
+}
