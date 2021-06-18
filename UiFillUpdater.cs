f995c7b97564b4dd4e587d252ec076ec707c2026
Baptiste <baptiste.manusset@gmail.com>
2021-06-18 19:13:29 +0200

    uifuill

--- UiFillUpdater.cs
+++ UiFillUpdater.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using Scriptabbles.ScriptableEvent;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class UiFillUpdater : MonoBehaviour {
     [Header("Image")] [SerializeField] private Image _image;
 
     [Header("References")] [SerializeField]
-    private GameEvent _gameEvent;
+    private GameEvent.GameEvent _gameEvent;
 
     private void Awake() { }
 }
