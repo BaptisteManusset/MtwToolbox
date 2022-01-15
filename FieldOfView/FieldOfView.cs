58b445ab952a10f512b01debb5b664c7f4a51562
Baptiste <baptiste.manusset@gmail.com>
2022-01-15 16:39:20 +0100

    field of view rework

--- FieldOfView/FieldOfView.cs
+++ FieldOfView/FieldOfView.cs
@@ -1,83 +1,89 @@
 using System;
-using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
+using ItsBaptiste.Core;
 using UnityEditor;
+using UnityEngine;
 
-public class FieldOfView : MonoBehaviour {
-    [Header("Parametres")] [Tooltip("Parametre changé dynamiquement durant le projet")] [SerializeField]
-    public FieldOfViewParam fovParam;
-
-
-    [SerializeField] protected List<Transform> visibleTargets = new List<Transform>();
-
-    public List<Transform> GetVisibleTargets() => visibleTargets;
+namespace ItsBaptiste.Toolbox.FieldOfView {
+    public class FieldOfView : MonoBehaviour {
+        [Header("Parametres")] [SerializeField] public FieldOfViewParam fovParam;
 
 
-    [Serializable]
-    public class FieldOfViewParam {
-        public float viewRadius;
-        [Range(0, 360)] public float viewAngle;
+        private List<Transform> visibleTargets = new List<Transform>();
 
+        [Serializable]
+        public class FieldOfViewParam {
+            public float viewRadius;
+            [Range(0, 360)] public float viewAngle;
+            public LayerMask targetMask;
+            public LayerMask obstacleMask;
+        }
 
-        public LayerMask targetMask;
-        public LayerMask obstacleMask;
-    }
+        public void SetFieldOfView(FieldOfViewParam fieldOfViewParam) {
+            fovParam = fieldOfViewParam;
+        }
 
+        public List<Transform> FindVisibleTargets() {
+            visibleTargets.Clear();
 
-    private void FixedUpdate() {
-        FindVisibleTargets();
-    }
+            var slt = Physics.OverlapSphere(transform.position, fovParam.viewRadius, fovParam.targetMask).ToList();
+            List<Collider> targetsInViewRadius = slt;
 
+            for (int i = 0; i < targetsInViewRadius.Count; i++) {
+                Transform target = targetsInViewRadius[i].transform;
+                Vector3 dirToTarget = (target.position - transform.position).normalized;
 
-    public void SetFieldOfView(FieldOfViewParam fieldOfViewParam) {
-        fovParam = fieldOfViewParam;
-    }
+                //verifie si l'on est dans la zone de vue
+                if (Vector3.Angle(transform.forward, dirToTarget) < fovParam.viewAngle / 2) {
+                    float dstToTarget = Vector3.Distance(transform.position, target.position);
 
-    private void FindVisibleTargets() {
-        visibleTargets.Clear();
-        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, fovParam.viewRadius,
-            fovParam.targetMask);
-
-        for (int i = 0; i < targetsInViewRadius.Length; i++) {
-            Transform target = targetsInViewRadius[i].transform;
-            Vector3 dirToTarget = (target.position - transform.position).normalized;
-            if (Vector3.Angle(transform.forward, dirToTarget) < fovParam.viewAngle / 2) {
-                float dstToTarget = Vector3.Distance(transform.position, target.position);
-                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, fovParam.obstacleMask)) {
-                    if (!visibleTargets.Contains(target))
-                        visibleTargets.Add(target);
+                    //verifie que la cible est visible 
+                    if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, fovParam.obstacleMask)) {
+                        if (!visibleTargets.Contains(target))
+                            visibleTargets.Add(target);
+                    }
                 }
             }
-        }
-    }
 
-    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) {
-        if (!angleIsGlobal) {
-            angleInDegrees += transform.eulerAngles.y;
+            return visibleTargets;
         }
 
-        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
-    }
+
+        public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) {
+            if (!angleIsGlobal) {
+                angleInDegrees += transform.eulerAngles.y;
+            }
+
+            return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+        }
 
 #if UNITY_EDITOR
+        void OnDrawGizmosSelected() {
+            //Draws view reach
+            Handles.color = Color.white;
+            var position = transform.position;
+            Handles.DrawWireArc(position, Vector3.up, Vector3.forward, 360, fovParam.viewRadius);
 
-    void OnDrawGizmosSelected() {
-        //Draws view reach
-        Handles.color = Color.white;
-        var position = transform.position;
-        Handles.DrawWireArc(position, Vector3.up, Vector3.forward, 360, fovParam.viewRadius);
+            //Draws cone of view
+            Vector3 viewAngleA = DirFromAngle(-fovParam.viewAngle / 2, false);
+            Vector3 viewAngleB = DirFromAngle(fovParam.viewAngle / 2, false);
+            Handles.DrawLine(position, position + viewAngleA * fovParam.viewRadius);
+            Handles.DrawLine(position, position + viewAngleB * fovParam.viewRadius);
 
-        //Draws cone of view
-        Vector3 viewAngleA = DirFromAngle(-fovParam.viewAngle / 2, false);
-        Vector3 viewAngleB = DirFromAngle(fovParam.viewAngle / 2, false);
-        Handles.DrawLine(position, position + viewAngleA * fovParam.viewRadius);
-        Handles.DrawLine(position, position + viewAngleB * fovParam.viewRadius);
+            Gizmos.color = Color.red;
+            foreach (Transform visibleTarget in visibleTargets) {
+                Gizmos.DrawLine(transform.position, visibleTarget.position);
+            }
+        }
+#endif
+    }
 
-        Gizmos.color = Color.red;
-        foreach (Transform visibleTarget in GetVisibleTargets()) {
-            Gizmos.DrawLine(transform.position, visibleTarget.position);
+    public static class FieldOfViewTools {
+        public static List<Transform> IsInAnotherTeam(this List<Transform> list, TeamsInfo.Team team) {
+            return list.Where(x =>
+                x.GetComponent<TeamsInfo>() != null &&
+                x.GetComponent<TeamsInfo>().GetTeam() != team).ToList();
         }
     }
-    #endif
-
 }
